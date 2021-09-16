72335519483db57304b4e755e63a8b0351f1b400
miyagawatakashi <miyagawa@hanjou.co.jp>
2021-09-16 22:26:07 +0900

    vue.js、bootstrapの反映テスト

---
+++ backend/resources/views/app.blade.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <!-- CSRF Token -->
+        <meta name="csrf-token" content={{ csrf_token() }}>
+        <title>{{ config('app.name', 'Vue Laravel SPA') }}</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+
+        <!-- Styles -->
+        <style>
+        </style>
+            <link href={{ mix('/css/app.css') }} rel="stylesheet">
+        <style>
+            
+        </style>
+    </head>
+    <body>
+        <div id="app">
+            <example-component></example-component>
+        </div>
+    </body>
+    <!-- Scripts -->
+    <script src="{{ mix('/js/app.js') }}" defer></script>
+</html>
