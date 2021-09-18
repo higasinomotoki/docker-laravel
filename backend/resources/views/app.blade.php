0fc343cb613437494d2c6137556392c9dfa1df67
miyagawatakashi <miyagawa@hanjou.co.jp>
2021-09-18 22:29:03 +0900

    VueComponentの実装

--- backend/resources/views/app.blade.php
+++ backend/resources/views/app.blade.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<html lang={{ str_replace('_', '-', app()->getLocale()) }}>
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -11,18 +11,14 @@
         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
 
         <!-- Styles -->
-        <style>
-        </style>
-            <link href={{ mix('/css/app.css') }} rel="stylesheet">
-        <style>
-            
-        </style>
+        <link href={{ mix('/css/app.css') }} rel="stylesheet">
     </head>
     <body>
         <div id="app">
             <header-component></header-component>
+            <router-view></router-view>
         </div>
     </body>
     <!-- Scripts -->
-    <script src="{{ mix('/js/app.js') }}" defer></script>
+    <script src={{ mix('/js/app.js') }} defer></script>
 </html>
