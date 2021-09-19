e0e5d71624377dff97e027dc17d61e9f1e2e1708
miyagawatakashi <miyagawa@hanjou.co.jp>
2021-09-19 22:25:14 +0900

    APIのcrud処理追加

---
+++ backend/app/Http/Controllers/TaskController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Task;
+
+class TaskController extends Controller
+{
+    public function index()
+    {
+        return Task::all();
+    }
+    public function show(Task $task)
+    {
+        return $task;
+    }
+    public function store(Request $request)
+    {
+        return Task::create($request->all());
+    }
+    public function update(Request $request, Task $task)
+    {
+        return $task;
+    }
+    public function destroy(Task $task)
+    {
+        return $task;
+    }
+}
